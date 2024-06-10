b3f5d6c97e91b7ff644dd39d10260329f9185a7d
Allan Kipkorir <allantuix@gmail.com>
2024-06-10 11:32:36 +0300

    Completed fetch request using useEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, {useEffect, useState} from 'react'
+
+function App() {
+    const [loading, setLoading] = useState(true);
+    const [imageUrl, setImageUrl] = useState(' ')
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+          .then(response => response.json())
+          .then(data => {
+            setImageUrl(data.message); // Update the image URL state
+            setLoading(false); // Update the loading state
+          })
+          .catch(error => {
+            console.error('Error fetching the dog image:', error);
+          });
+      }, []); // Empty dependency array ensures this effect runs only once
+    
+      return (
+        <div>
+            {loading ? (<p>Loading ...</p>) : (<img src={imageUrl} alt="A Random Dog"/>)}
+        </div>
+      )
+
+
+}
+
+export default App
